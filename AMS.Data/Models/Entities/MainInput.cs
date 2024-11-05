4af6b977957bb34428b8939e82e2d6b7f625d9eb
Anubis <carloboado18@gmail.com>
2024-11-06 03:13:53 +0800

    Added Main Input UI

---
+++ AMS.Data/Models/Entities/MainInput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMS.Data.Models.Entities
+{
+	public class EnergyInput
+	{
+		public int Id { get; set; }
+		public DateTime? ReadingDate { get; set; }
+		public ReadingInput ReadingInput { get; set; } = new();
+		public int? UnitNumber { get; set; }		
+		public decimal TotalKwhUsed { get; set; }
+		public decimal TotalComsumption { get; set; }
+		public bool IsMainInput { get; set; } = true;
+
+	}
+
+
+	public partial class ReadingInput : BaseModel
+	{
+		public decimal PreviousReadingKwh { get; set; } = 0.00M;
+		public decimal PresentReadingKwh { get; set; } = 0.00M;
+		public decimal PreviousConsumption { get; set; } = 0.00M;
+		public decimal PresentConsumption { get; set; } = 0.00M;
+	}
+
+
+}
---
+++ AMS.Web/Components/Pages/Inputs/MainInput.razor.cs
@@ -0,0 +1,100 @@
+﻿using AMS.Data.Models.Entities;
+using Microsoft.AspNetCore.Components;
+using AMS.Web.Components.Pages.Inputs.Components;
+using MudBlazor;
+using AMS.Data.Models;
+using AMS.Web.Components.Pages.Management.Components;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using AMS.Web.Components.Pages.Management;
+namespace AMS.Web.Components.Pages.Inputs
+{
+	public partial class MainInput : ComponentBase
+	{
+		[Inject]
+        IDialogService DialogService { get; set; }
+        [Inject] ProtectedSessionStorage _sessionStorage { get; set; }
+        private List<EnergyInput> mainEnergyInput = new();
+		private bool _loading = false;
+
+        DialogOptions options = new DialogOptions()
+        {
+            CloseOnEscapeKey = true,
+            BackdropClick = false,
+            Position = DialogPosition.TopCenter,
+            BackgroundClass = "dialogBlur",
+            FullWidth = true,
+            CloseButton = true,
+            MaxWidth = MaxWidth.Small
+        };
+
+        protected override async Task OnInitializedAsync()
+		{
+			var energyInput1 = new EnergyInput
+			{
+				Id = 1,
+				ReadingDate = DateTime.Now,
+				ReadingInput = new ReadingInput
+				{
+					PreviousReadingKwh = 500.00M,
+					PresentReadingKwh = 550.00M,
+					PreviousConsumption = 200.00M,
+					PresentConsumption = 50.00M,
+					CreatedBy = "User1",
+					CreatedOn = DateTime.Now.AddMonths(-1),
+					UpdatedBy = "User1",
+					UpdatedOn = DateTime.Now
+				},
+				UnitNumber = 101,
+				TotalKwhUsed = 50.00M,
+				TotalComsumption = 250.0000M
+			};
+			mainEnergyInput.Add(energyInput1);
+
+			var energyInput2 = new EnergyInput
+			{
+				Id = 2,
+				ReadingDate = DateTime.Now,
+				ReadingInput = new ReadingInput
+				{
+					PreviousReadingKwh = 1000.00M,
+					PresentReadingKwh = 1100.00M,
+					PreviousConsumption = 400.00M,
+					PresentConsumption = 100.00M,
+					CreatedBy = "User2",
+					CreatedOn = DateTime.Now.AddMonths(-2),
+					UpdatedBy = "User2",
+					UpdatedOn = DateTime.Now
+				},
+				UnitNumber = 102,
+				TotalKwhUsed = 100.00M,
+				TotalComsumption = 500.00M
+			};
+
+			mainEnergyInput.Add(energyInput2);
+
+			await base.OnInitializedAsync();
+		}
+
+
+		private async Task OnShowInputDialog(EnergyInput input, string action)
+		{
+			var session = await _sessionStorage.GetAsync<UserSession>("UserSession");
+            var userId = session.Value.Id;
+            var parameters = new DialogParameters<EnergyInputDialog>()
+            {
+                {x=>x.Action, action},
+                {x=>x.userId, userId},
+                {x=>x.input, input}
+
+            };
+            var dialog = await DialogService.ShowAsync<EnergyInputDialog>("Simple Dialog", parameters, options);
+            var result = await dialog.Result;
+            if (!result.Canceled)
+            {
+                
+            }
+        }
+
+
+	}
+}
